10930a792db467e5403becafdf1fb46ebca3a2b2
Jayden <jayden@chequer.io>
2019-12-13 10:43:29 +0900

    Google 로그인 구현

--- src/main/java/com/jayden/springboot/web/IndexController.java
+++ src/main/java/com/jayden/springboot/web/IndexController.java
@@ -1,22 +1,33 @@
 package com.jayden.springboot.web;
 
+import com.jayden.springboot.config.auth.dto.SessionUser;
 import com.jayden.springboot.service.posts.PostsService;
 import com.jayden.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
