917229fdceec22df04a5992da6834e8055c1a75b
jayden-lee <gwanggi.lee@gmail.com>
2019-12-17 10:05:04 +0900

    Naver 로그인 구현

--- src/main/java/com/jayden/springboot/web/IndexController.java
+++ src/main/java/com/jayden/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jayden.springboot.web;
 
+import com.jayden.springboot.config.auth.LoginUser;
 import com.jayden.springboot.config.auth.dto.SessionUser;
 import com.jayden.springboot.service.posts.PostsService;
 import com.jayden.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
