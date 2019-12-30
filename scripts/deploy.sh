83a19bafb63fa733b221f7e050d0077b94ff0163
jayden-lee <gwanggi.lee@gmail.com>
2019-12-30 23:08:18 +0900

    배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,8 +30,8 @@ echo "현재 실행중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 실행중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo ">kill -15 $CURRENT_PID"
-    kill -15 $CURRNET_PID
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 5
 fi
 
