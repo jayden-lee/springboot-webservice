683e41fd2c717fab90c287323ea5c107607fd79d
jayden-lee <gwanggi.lee@gmail.com>
2020-01-01 15:35:38 +0900

    배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step1
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot-webservice
-
-cd $REPOSITORY/$PROJECT_NAME/
-
-echo "> Git Pull"
-
-git pull
-
-echo "> 프로젝트 Build 시작"
-
-./gradlew build
-
-echo "> step1 디렉토리로 이동"
-
-cd $REPOSITORY
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 실행중인 애플리케이션 pid 확인"
 
